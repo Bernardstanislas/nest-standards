5cf14a7d180c212cdb5fbc79bf7e96d1ab9ce619
Stanislas Bernard <bernardstanislas@gmail.com>
2021-03-04 12:30:49 +0100

    :sparkles: Add faulty code to write testing standard

---
+++ src/dinosaur/dinosaur.service.spec.ts
@@ -0,0 +1,9 @@
+import { DinosaurService } from 'src/dinosaur/dinosaur.service';
+
+describe('The dinosaur service', () => {
+  let service: DinosaurService;
+
+  it('exists', () => {
+    expect(service).toBeDefined();
+  });
+});
---
+++ src/dinosaur/dinosaur.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Dinosaur } from 'src/dinosaur/dinosaur.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class DinosaurService {
+  constructor(
+    @InjectRepository(Dinosaur) private readonly dinoRepo: Repository<Dinosaur>,
+  ) {}
+}
